e03afd34506450e73c39151c54b5ac7abc1f24dc
renovatia <akanshspark@gmail.com>
2022-07-28 12:30:34 +0530

    added new LambdaFile.

--- AdvancedPythonTutorial/lambdaOperator.py
+++ AdvancedPythonTutorial/lambdaOperator.py
@@ -4,15 +4,22 @@
 # map Function
 # r = map(func, seq)
 
-def fahrenheit(T):
-    return ((float(9)/5)*T +32)
-def celsius (T):
-    return ((float(5)/9)*(T-32))
-temperatures = (36.5, 37, 37.5, 38, 39)
+# def fahrenheit(T):
+#     return ((float(9)/5)*T +32)
+# def celsius (T):
+#     return ((float(5)/9)*(T-32))
+# temperatures = (36.5, 37, 37.5, 38, 39)
+#
+# returnFah = list(map(fahrenheit,temperatures))
+# returnCels = list(map(celsius,temperatures))
+#
+# print(returnFah)
+# print(returnCels)
 
-returnFah = list(map(fahrenheit,temperatures))
-returnCels = list(map(celsius,temperatures))
+# c = [39.2, 36.5, 37.3, 38, 37.8]
+# f = list(map(lambda x: ((float(9)/5)*x+32),c))
+# print(f)
 
-print(returnFah)
-print(returnCels)
-
+# c = [39.2, 36.5, 37.3, 38, 37.8]
+# f = list(map(lambda x: (float(5/9)*(x-32)),c))
+# print(f)
